641a74a9a36016590d64cc77b98223c2d78b59ef
Rich Trott <rtrott@gmail.com>
2025-03-06 14:57:32 -0800

    chore: update to new API

--- lunchbot.js
+++ lunchbot.js
@@ -1,13 +1,11 @@
 'use strict'
 
 if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
-const foursquare = require('node-foursquare-venues')(process.env.FOURSQUARE_CLIENT_ID, process.env.FOURSQUARE_CLIENT_SECRET)
-
-const search = require('util').promisify(foursquare.venues.search)
+const fsqDevelopers = require('@api/fsq-developers')
 
 const lunchSearchObj = {
-  near: '530 Parnassus Avenue San Francisco CA',
-  categoryId: [
+  ll: '37.7634643,-122.4591061',
+  categories: [
     '4bf58dd8d48988d142941735', // Asian (Nan King, Soi Gow, New Sandy's, etc.)
     '4bf58dd8d48988d1bd941735', // Salad (Pluto's)
     '4bf58dd8d48988d1c5941735', // Sandwich (Wooly Pig)
@@ -18,20 +16,18 @@ const lunchSearchObj = {
     '4bf58dd8d48988d10f941735', // Indian
     '4bf58dd8d48988d1c1941735' // Mexican
   ].join(','),
-  radius: 625,
   limit: 50,
-  v: '20231010'
+  radius: 625
 }
 
 const coffeeSearchObj = {
-  near: '530 Parnassus Avenue San Francisco CA',
-  categoryId: [
+  ll: '37.7634643,-122.4591061',
+  categories: [
     '4bf58dd8d48988d1e0931735', // Coffee Shop
     '5665c7b9498e7d8a4f2c0f06' // Corporate Coffee Shop
   ].join(','),
-  radius: 300,
   limit: 50,
-  v: '20231010'
+  radius: 300
 }
 
 module.exports = async (searchType) => {
@@ -49,8 +45,9 @@ module.exports = async (searchType) => {
   }
 
   try {
-    const payload = await search(searchObj)
-    const recs = payload.response.venues
+    fsqDevelopers.auth(process.env.FOURSQUARE_API_TOKEN)
+    const payload = await fsqDevelopers.placeSearch(searchObj)
+    const recs = payload.data.results
     const rec = recs[Math.floor(Math.random() * recs.length)]
     const url = 'https://www.foursquare.com/v/'
 
@@ -59,7 +56,7 @@ module.exports = async (searchType) => {
     }
 
     message = rec.name + ' (' + rec.location.address + ')\n'
-    message += url + rec.id
+    message += url + rec.fsq_id
 
     message += `\n\n${messageTag}`
   } catch (error) {
