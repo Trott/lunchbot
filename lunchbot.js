51da7ddc4d2327ddaa7734c2a291febf17321a20
Rich Trott <rtrott@gmail.com>
2026-01-01 14:12:22 -0800

    chore: bump dotenv from 16.x to 17.x

--- lunchbot.js
+++ lunchbot.js
@@ -1,6 +1,6 @@
 'use strict'
 
-if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config({ quiet: true }) }
 
 const lunchSearchObj = {
   ll: '37.7634643,-122.4591061',
